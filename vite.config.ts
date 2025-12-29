455f54603398f2461b05f228997f840fcd54f060
AriffZack <A193193@siswa.ukm.edu.my>
2025-12-29 17:08:52 +0800

    Changing vite configuration settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000, // Let's give Admin the "3000" penthouse
-    strictPort: true, // Fail if 3000 is taken (so you know something is wrong)
-  }
-})
+// We change this to a function so we can check if we are in 'production' mode
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    
+    server: {
+      port: 3000,      
+      strictPort: true, 
+    },
+
+    
+    esbuild: {
+      // If building for production, remove console.log, .debug, and .info
+      // If in dev, keep them so you can see what you are doing
+      pure: mode === 'production' ? ['console.log', 'console.debug', 'console.info'] : [],
+    },
+  };
+});
